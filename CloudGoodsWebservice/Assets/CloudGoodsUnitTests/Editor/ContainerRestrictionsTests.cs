eb0b5ed090cf98726807a7ddd60db287896e4d2e
Lionel Sy <lionely.sy@gmail.com>
2015-03-11 12:15:29 -0400

    Visual Studio 2013 update

---
+++ CloudGoodsWebservice/Assets/CloudGoodsUnitTests/Editor/ContainerRestrictionsTests.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+public class ContainerRestrictionsTests : MonoBehaviour {
+
+	
+}
